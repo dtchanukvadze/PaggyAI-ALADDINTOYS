9352c525631d3ae7f82577708deed982f54e78dc
dtchanukvadze <chanuyvadzedat@gmail.com>
2026-06-01 08:10:58 +0400

    Admin Auth System Update

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,7 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV !== 'production';
-
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            // If in development, allow 'unsafe-eval' so hot-reload works.
-            // If in production, remove 'unsafe-eval' for strict security.
-            value: isDev 
-              ? "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-              : "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';"
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
